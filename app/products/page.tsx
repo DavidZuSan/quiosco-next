4ab14edb42a7d8234da2cd12aa755fe5e1c73f61
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-10-07 17:25:19 +0200

    Quiosco de Comida

---
+++ app/products/page.tsx
@@ -0,0 +1,6 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function ProductsPage() {
+  console.log("Desde el servidor");
+  return <Sidebar />;
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function Sidebar() {
+  console.log("Desde el Sidebar");
+  return <div>Sidebar</div>;
+}
