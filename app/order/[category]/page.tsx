c85363e4e5b1ed1bcfbd164efe3fac923a55245e
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-10-09 15:07:26 +0200

    Categorias y Productos

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -21,6 +21,9 @@ export default async function OrderPage({
 
   return (
     <>
+      <h1 className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </h1>
       <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 gap-4 items-start">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
--- app/order/layout.tsx
+++ app/order/layout.tsx
@@ -3,16 +3,16 @@ import OrderSummary from "@/components/order/OrderSummary";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       <div className="md:flex">
         <OrderSidebar />
+
         <main className="md:flex-1 md:h-screen md:overflow-y-scroll p-5">
           {children}
         </main>
+
         <OrderSummary />
       </div>
     </>
