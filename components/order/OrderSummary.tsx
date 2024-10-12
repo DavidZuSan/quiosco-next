4d0f2dd719329633373a862f296ead89a8d4b073
David Zurita <lacosadelumbral@gmail.com>
2024-10-12 19:26:06 +0200

    Almacenamiento de Ordenes

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,24 +1,44 @@
 "use client";
+import { useMemo } from "react";
+import { toast } from "react-toastify";
 import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
-import { useMemo } from "react";
 import { formatCurrency } from "@/src/utils";
 import { createOrder } from "@/actions/create-order-action";
 import { OrderSchema } from "@/src/schema";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
+  const clearOrder = useStore((state) => state.clearOrder);
   const total = useMemo(
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
     [order]
   );
 
-  const handleCreateOrder = (formData: FormData) => {
+  const handleCreateOrder = async (formData: FormData) => {
     const data = {
       name: formData.get("name"),
+      total,
+      order,
     };
+
     const result = OrderSchema.safeParse(data);
-    createOrder();
+    if (!result.success) {
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+
+    const response = await createOrder(data);
+    if (response?.errors) {
+      response.errors.forEach((issue) => {
+        toast.error(issue.message);
+      });
+    }
+
+    toast.success("Pedido Realizado Correctamente");
+    clearOrder();
   };
 
   return (
@@ -32,8 +52,9 @@ export default function OrderSummary() {
           {order.map((item) => (
             <ProductDetails key={item.id} item={item} />
           ))}
+
           <p className="text-2xl mt-20 text-center">
-            Total a pagar: {""}{" "}
+            Total a pagar: {""}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
 
@@ -44,6 +65,7 @@ export default function OrderSummary() {
               className="bg-white border border-gray-100 p-2 w-full"
               name="name"
             />
+
             <input
               type="submit"
               className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
