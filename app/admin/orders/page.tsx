0e28778dc51a938de81ce1333bae3a0feb82ae1b
David Zurita <lacosadelumbral@gmail.com>
2024-10-14 13:32:55 +0200

    Administrador de Productos - I

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
@@ -25,7 +26,9 @@ export default async function Orderspage() {
       <Heading>Administrar Ordenes</Heading>
       {orders.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map(order => ())}
+          {orders.map((order) => (
+            <OrderCard key={order.id} order={order} />
+          ))}
         </div>
       ) : (
         <p className="text-center">No hay ordenees Pendientes</p>
